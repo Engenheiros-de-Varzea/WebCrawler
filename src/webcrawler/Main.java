1fc2668223ca9cd2aaf6c8db7cd4be017733a363
PHCarmo <paulo.henrique.carmo@hotmail.com>
2020-03-14 19:38:03 -0300

    Tratamento dos JSON's em List's adicionado

--- src/webcrawler/Main.java
+++ src/webcrawler/Main.java
@@ -1,6 +1,11 @@
 package webcrawler;
 
+import webcrawler.control.Request;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import webcrawler.control.JSON;
+import webcrawler.model.bean.ServidorBean;
 
 /**
  *
@@ -10,8 +15,16 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Request.getRequest("http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2");
-        Request.getRequest("http://www.licitacao.pmmc.com.br/Transparencia/detalhamento?rgf=19675");
+        String url = "http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2";
+        List<Object> rgfs = JSON.tratarRgfs(JSON.converter(Request.getRequest(url)));
+        
+        List<ServidorBean> servidores = new ArrayList<>();
+        for(Object rgf: rgfs){
+            url = "http://www.licitacao.pmmc.com.br/Transparencia/detalhamento?rgf=" + rgf;
+            servidores.add(JSON.tratarServidor(JSON.converter(Request.getRequest(url)), rgf));
+        }
+        
+        //INSERT servidores
     }
     
 }
